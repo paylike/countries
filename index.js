ab535e1dc5e9857437c5cec2d17b609e6c809f04
misozask <michal@paylike.io>
2024-05-14 11:35:36 +0200

    Switch to ESM

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-'use strict';
 const countries = [
 	{code: 'DK', currency: 'DKK', name: 'Denmark'},
 	{code: 'NO', currency: 'NOK', name: 'Norway'},
 	{code: 'SE', currency: 'SEK', name: 'Sweden'},
 ]
 
-
-module.exports = countries;
+export default countries
